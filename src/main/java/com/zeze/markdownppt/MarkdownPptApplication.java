5ec7dc9b81e192e8cf4e3aa475c17effd7c51b34
lowoon <sangbo0229@gmail.com>
2020-07-07 17:44:18 +0900

    init

---
+++ src/main/java/com/zeze/markdownppt/MarkdownPptApplication.java
@@ -0,0 +1,13 @@
+package com.zeze.markdownppt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkdownPptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkdownPptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zeze/markdownppt/MarkdownPptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeze.markdownppt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkdownPptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
